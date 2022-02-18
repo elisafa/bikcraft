a0874ecdb98a6124b6a14ab77595cff9540cdc4d
Elisafa <elisafajunior@gmail.com>
2022-02-18 15:09:17 -0300

    JavaScript nas perguntas do seguro

--- js/script.js
+++ js/script.js
@@ -19,5 +19,24 @@ function ativarProduto(parametro){
     elemento.checked = true;
   }
 }
-
 parametros.forEach(ativarProduto);
+
+// Perguntas frequentes
+const perguntas = document.querySelectorAll('.perguntas button')
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls')
+  const resposta = document.getElementById(controls)
+  resposta.classList.toggle('ativo')
+  const ativa = resposta.classList.contains(ativa)
+  pergunta.setAttribute('aria-expanded', 'true')
+}
+
+
+function eventosPerguntas(pergunta){
+  pergunta.addEventListener('click', ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
